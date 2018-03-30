13f781ae03e7ab877dc201c17c11e8e4f5701086
민경운 <min_it@platfos.com>
2018-03-30 16:54:54 +0900

    1. 프로젝트생성

---
+++ src/test/java/com/minit/webservice1/SpringWebservice1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.minit.webservice1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebservice1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
