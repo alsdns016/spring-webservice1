a37bb478636042bff284bb66bf42d8d6c57ece42
민경운 <min_it@platfos.com>
2018-03-30 17:12:18 +0900

    1. Chapter2 완료

--- src/main/java/com/minit/webservice1/web/WebRestController.java
+++ src/main/java/com/minit/webservice1/web/WebRestController.java
@@ -1,14 +1,27 @@
 package com.minit.webservice1.web;
 
+import com.minit.webservice1.dto.posts.PostsSaveRequestDto;
+import com.minit.webservice1.posts.PostsRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello(){
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 
 }
